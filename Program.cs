12c61d39d4e7a1d939d1439c9d64dd6adc6c22e2
mer1ch <mericburakacar@hotmail.com>
2025-07-23 09:32:19 +0300

    Starting Questions

---
+++ Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode_Solutions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region 01. Two Sum
+
+        //    public class Solution
+        //{
+        //    public int[] TwoSum(int[] nums, int target)
+        //    {
+        //        for (int i = 0; i < nums.Length; i++)
+        //        {
+        //            for (int j = 0; j < nums.Length; j++)
+        //            {
+        //                if (nums[i] + nums[j] == target)
+        //                {
+        //                    if (i != j)
+        //                    {
+        //                        return [i, j];
+        //                        break;
+        //                    }
+        //                }
+        //            }
+        //        }
+        //        return [0, 0];
+        //    }
+        //}
+
+        #endregion
+
+        #region 09. Palindrome
+
+        //    public class Solution
+        //{
+        //    public bool IsPalindrome(int x)
+        //    {
+        //        bool isPalindrome = false;
+        //        string s = x.ToString();
+        //        for (int i = 0; i < s.Length; i++)
+        //        {
+        //            if (s[i] == s[(s.Length - 1) - i])
+        //            {
+        //                isPalindrome = true;
+        //            }
+        //            else
+        //            {
+        //                isPalindrome = false;
+        //                break;
+        //            }
+        //        }
+        //        return isPalindrome;
+        //    }
+        //}
+
+        #endregion
+    }
+    }
+}
